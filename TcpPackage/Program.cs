0d4d451de58ac961c80769c4e4720fc5b5c9e76d
pinopino <myhinata@126.com>
2018-10-12 10:41:15 +0800

    hotfix-清理program.cs

--- TcpPackage/Program.cs
+++ TcpPackage/Program.cs
@@ -12,26 +12,17 @@ namespace Incubator.TcpPackage
             //TestSuits.FunD();
             //TestSuits.FunE();
 
-            ////=== recv < head ===
-            //TestSuits2.FunA();
-            ////=== recv = head ===
-            //TestSuits2.FunB();
-            ////=== recv < head + message ===
-            //TestSuits2.FunC();
-            ////=== recv = head + message ===
-            //TestSuits2.FunD();
-            ////=== recv > head + message ===
+            //=== recv < head ===
+            TestSuits2.FunA();
+            //=== recv = head ===
+            TestSuits2.FunB();
+            //=== recv < head + message ===
+            TestSuits2.FunC();
+            //=== recv = head + message ===
+            TestSuits2.FunD();
+            //=== recv > head + message ===
             TestSuits2.FunE();
             Console.Read();
-        }
-
-        static void f(byte[] d)
-        {
-            System.Threading.Thread.Sleep(3000);
-
-            Console.WriteLine(d[0]);
-            Console.WriteLine(d[1]);
-            Console.WriteLine(d[2]);
         }
     }
 }
