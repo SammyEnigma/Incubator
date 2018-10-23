a325615fe5aa7a0c3b9256716641e2868b019add
pinopino <myhinata@126.com>
2018-10-23 17:17:18 +0800

    初步引入rpc试验代码

---
+++ SocketClient/Rpc/RpcConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Incubator.SocketClient.Rpc
+{
+    public class RpcConnection : ClientConnectionBase
+    {
+        public RpcConnection(string address, int port, int bufferSize, bool debug = false) 
+            : base(address, port, bufferSize, debug)
+        {
+        }
+    }
+}
