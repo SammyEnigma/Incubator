4df95e6b8d48af32ed300ab88f4327334b2c5238
pinopino <myhinata@126.com>
2018-10-24 17:42:44 +0800

    1. rpc client重构 2. client端调用远程方法代码初步完成

--- SocketClient/Rpc/RpcClient.cs
+++ SocketClient/Rpc/RpcClient.cs
@@ -1,11 +1,155 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Incubator.SocketServer;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
 using System.Text;
 
-namespace SocketClient.Rpc
+namespace Incubator.SocketClient.Rpc
 {
-    public class RpcClient
+    public interface IRpcCall
     {
+        /// <summary>
+        /// Invokes the method with the specified parameters.
+        /// </summary>
+        /// <param name="parameters">Parameters for the method call</param>
+        /// <returns>An array of objects containing the return value (index 0) and the parameters used to call
+        /// the method, including any marked as "ref" or "out"</returns>
+        object[] InvokeMethod(string metaData, params object[] parameters);
 
+        /// <summary>
+        /// Channel must implement an interface synchronization method.
+        /// This method asks the server for a list of identifiers paired with method
+        /// names and -parameter types. This is used when invoking methods server side.
+        /// When username and password supplied, zero knowledge encryption is used.
+        /// </summary>
+        void SyncInterface(Type serviceType);
+    }
+
+    public class RpcClient : IRpcCall
+    {
+        bool _debug;
+        object _syncRoot;
+        MemoryStream _stream;
+        BinaryWriter _binWriter;
+        ServiceSyncInfo _syncInfo;
+        ObjectPool<IPooledWapper> _connectionPool;
+        ParameterTransferHelper _parameterTransferHelper;
+        // keep cached sync info to avoid redundant wire trips
+        private static ConcurrentDictionary<Type, ServiceSyncInfo> _syncInfoCache = new ConcurrentDictionary<Type, ServiceSyncInfo>();
+
+        public RpcClient(string address, int port, bool debug)
+        {
+            _debug = debug;
+            _syncRoot = new object();
+            _stream = new MemoryStream(512);
+            _binWriter = new BinaryWriter(_stream, Encoding.UTF8);
+            _parameterTransferHelper = new ParameterTransferHelper();
+            _connectionPool = new ObjectPool<IPooledWapper>(12, 4, pool => new RpcConnection(pool, address, port, 256, _debug));
+        }
+
+        class SyncInterfaceInfo
+        {
+            public int MessageType;
+            // SyncInterface
+            public string FullName;
+            // MethodInvocation
+            public int ServiceKeyIndex;
+            public int MethodIdent;
+        }
+
+        public void SyncInterface(Type serviceType)
+        {
+            if (!_syncInfoCache.TryGetValue(serviceType, out _syncInfo))
+            {
+                using (var conn = ((RpcConnection)_connectionPool.Get()))
+                {
+                    conn.OnMessageReceived += (sender, e) =>
+                    {
+                        _syncInfo = e.MessageData.ToDeserializedObject<ServiceSyncInfo>();
+                        _syncInfoCache.AddOrUpdate(serviceType, _syncInfo, (t, info) => _syncInfo);
+                        _binWriter.Seek(0, SeekOrigin.Begin);
+                    };
+                    conn.Connect();
+                    _binWriter.Write((int)MessageType.SyncInterface);
+                    _binWriter.Write(serviceType.FullName);
+                    conn.Send(_stream.GetBuffer(), (int)_stream.Position);
+                }
+            }
+        }
+
+        public object[] InvokeMethod(string metaData, params object[] parameters)
+        {
+            //prevent call to invoke method on more than one thread at a time
+            lock (_syncRoot)
+            {
+                var mdata = metaData.Split('|');
+
+                //find the matching server side method ident
+                var ident = -1;
+                for (int index = 0; index < _syncInfo.MethodInfos.Length; index++)
+                {
+                    var si = _syncInfo.MethodInfos[index];
+                    //first of all the method names must match
+                    if (si.MethodName == mdata[0])
+                    {
+                        //second of all the parameter types and -count must match
+                        if (mdata.Length - 1 == si.ParameterTypes.Length)
+                        {
+                            var matchingParameterTypes = true;
+                            for (int i = 0; i < si.ParameterTypes.Length; i++)
+                                if (!mdata[i + 1].Equals(si.ParameterTypes[i].FullName))
+                                {
+                                    matchingParameterTypes = false;
+                                    break;
+                                }
+                            if (matchingParameterTypes)
+                            {
+                                ident = si.MethodIdent;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (ident < 0)
+                    throw new Exception(string.Format("Cannot match method '{0}' to its server side equivalent", mdata[0]));
+
+                using (var conn = ((RpcConnection)_connectionPool.Get()))
+                {
+                    conn.OnMessageReceived += (sender, e) =>
+                    {
+                        using (MemoryStream stream = new MemoryStream(e.MessageData))
+                        using (BinaryReader br = new BinaryReader(stream))
+                        {
+                            // Read the result of the invocation.
+                            MessageType messageType = (MessageType)br.ReadInt32();
+                            if (messageType == MessageType.UnknownMethod)
+                                throw new Exception("Unknown method.");
+
+                            object[] outParams = _parameterTransferHelper.ReceiveParameters(br);
+
+                            if (messageType == MessageType.ThrowException)
+                                throw (Exception)outParams[0];
+                        }
+                    };
+                    conn.Connect();
+                    // write the message type
+                    _binWriter.Write((int)MessageType.MethodInvocation);
+                    // write service key index
+                    _binWriter.Write(_syncInfo.ServiceKeyIndex);
+                    // write the method ident to the server
+                    _binWriter.Write(ident);
+                    // send the parameters
+                    _parameterTransferHelper.SendParameters(_syncInfo.UseCompression,
+                        _syncInfo.CompressionThreshold,
+                        _binWriter,
+                        parameters);
+
+                    conn.Send(_stream.GetBuffer(), (int)_stream.Position);
+                }
+
+                return null;
+            }
+        }
     }
 }
--- SocketClient/Rpc/RpcConnection.cs
+++ SocketClient/Rpc/RpcConnection.cs
@@ -1,150 +1,59 @@
 ﻿using Incubator.SocketServer;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Concurrent;
-using System.IO;
 
 namespace Incubator.SocketClient.Rpc
 {
-    public interface IRpcCall
-    {
-        /// <summary>
-        /// Invokes the method with the specified parameters.
-        /// </summary>
-        /// <param name="parameters">Parameters for the method call</param>
-        /// <returns>An array of objects containing the return value (index 0) and the parameters used to call
-        /// the method, including any marked as "ref" or "out"</returns>
-        object[] InvokeMethod(string metaData, params object[] parameters);
-
-        /// <summary>
-        /// Channel must implement an interface synchronization method.
-        /// This method asks the server for a list of identifiers paired with method
-        /// names and -parameter types. This is used when invoking methods server side.
-        /// When username and password supplied, zero knowledge encryption is used.
-        /// </summary>
-        void SyncInterface(Type serviceType);
-    }
-
-    public class RpcConnection : ClientConnectionBase, IRpcCall, IPooledWapper
-    {
-        object _syncRoot = new object();
+    public sealed class RpcConnection : ClientConnectionBase, IPooledWapper
+    {        
         bool _disposed;
-        Type _serviceType;
-        BinaryReader _binReader;
-        BinaryWriter _binWriter;
-        ParameterTransferHelper _parameterTransferHelper = new ParameterTransferHelper();
-        ServiceSyncInfo _syncInfo;
-
         ObjectPool<IPooledWapper> _pool;
         public DateTime LastGetTime { set; get; }
+        public bool IsDisposed { get { return _disposed; } }
+        public event EventHandler<Package> OnMessageReceived;
 
-        public bool IsDisposed => this._disposed;
-
-        // keep cached sync info to avoid redundant wire trips
-        private static ConcurrentDictionary<Type, ServiceSyncInfo> _syncInfoCache = new ConcurrentDictionary<Type, ServiceSyncInfo>();
-
-        public RpcConnection(
-            ObjectPool<IPooledWapper> pool,
-            string address, int port, int bufferSize, bool debug = false)
+        public RpcConnection(ObjectPool<IPooledWapper> pool, string address, int port, int bufferSize, bool debug = false)
             : base(address, port, bufferSize, debug)
         {
+            if (pool == null)
+                throw new ArgumentNullException("pool不能为空");
+            _disposed = false;
             _pool = pool;
         }
 
-        public object[] InvokeMethod(string metaData, params object[] parameters)
+        ~RpcConnection()
         {
-            //prevent call to invoke method on more than one thread at a time
-            lock (_syncRoot)
+            //必须为false
+            Dispose(false);
+        }
+
+        protected override void MessageReceived(byte[] messageData, int length)
+        {
+            OnMessageReceived?.Invoke(this, new Package { Connection = null, MessageData = messageData, DataLength = length });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
             {
-                var mdata = metaData.Split('|');
-
-                //write the message type
-                _binWriter.Write((int)MessageType.MethodInvocation);
-
-                //find the matching server side method ident
-                var ident = -1;
-                for (int index = 0; index < _syncInfo.MethodInfos.Length; index++)
+                return;
+            }
+            if (disposing)
+            {
+                // 清理托管资源
+                if (_pool.IsDisposed)
                 {
-                    var si = _syncInfo.MethodInfos[index];
-                    //first of all the method names must match
-                    if (si.MethodName == mdata[0])
-                    {
-                        //second of all the parameter types and -count must match
-                        if (mdata.Length - 1 == si.ParameterTypes.Length)
-                        {
-                            var matchingParameterTypes = true;
-                            for (int i = 0; i < si.ParameterTypes.Length; i++)
-                                if (!mdata[i + 1].Equals(si.ParameterTypes[i].FullName))
-                                {
-                                    matchingParameterTypes = false;
-                                    break;
-                                }
-                            if (matchingParameterTypes)
-                            {
-                                ident = si.MethodIdent;
-                                break;
-                            }
-                        }
-                    }
+                    base.Dispose();
                 }
-
-                if (ident < 0)
-                    throw new Exception(string.Format("Cannot match method '{0}' to its server side equivalent", mdata[0]));
-
-                //write service key index
-                _binWriter.Write(_syncInfo.ServiceKeyIndex);
-
-                //write the method ident to the server
-                _binWriter.Write(ident);
-
-                //send the parameters
-                _parameterTransferHelper.SendParameters(_syncInfo.UseCompression,
-                    _syncInfo.CompressionThreshold,
-                    _binWriter,
-                    parameters);
-
-                _binWriter.Flush();
-
-                // Read the result of the invocation.
-                MessageType messageType = (MessageType)_binReader.ReadInt32();
-                if (messageType == MessageType.UnknownMethod)
-                    throw new Exception("Unknown method.");
-
-                object[] outParams;
-                outParams = _parameterTransferHelper.ReceiveParameters(_binReader);
-
-                if (messageType == MessageType.ThrowException)
-                    throw (Exception)outParams[0];
-
-                return outParams;
+                else
+                {
+                    _pool.Put(this);
+                }
             }
-        }
 
-        class SyncInterfaceInfo
-        {
-            public int Type;
-            public string FullName;
-        }
+            // 清理非托管资源
 
-        public void SyncInterface(Type serviceType)
-        {            
-            if (!_syncInfoCache.TryGetValue(serviceType, out _syncInfo))
-            {
-                var obj = new SyncInterfaceInfo { Type = (int)MessageType.SyncInterface, FullName = serviceType.FullName };
-                //write the message type                
-                //_binWriter.Write((int)MessageType.SyncInterface);
-                //_binWriter.Write(serviceType.FullName);
-                Send(JsonConvert.SerializeObject(obj));
-
-                //read sync data
-                var len = _binReader.ReadInt32();
-                //len is zero when AssemblyQualifiedName not same version or not found
-                if (len == 0) throw new TypeAccessException("SyncInterface failed. Type or version of type unknown.");
-                var bytes = _binReader.ReadBytes(len);
-
-                _syncInfo = bytes.ToDeserializedObject<ServiceSyncInfo>();
-                _syncInfoCache.AddOrUpdate(serviceType, _syncInfo, (t, info) => _syncInfo);
-            }
+            // 让类型知道自己已经被释放
+            _disposed = true;
         }
     }    
 }
