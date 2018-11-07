c6c6c19ee8aabd4c94997773eb1e291f7666596a
pinopino <myhinata@126.com>
2018-11-07 15:24:01 +0800

    rpc generator0.4

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,45 +1,9 @@
 ﻿using Incubator.RpcContract;
-using Incubator.SocketClient.Rpc;
 using System;
 using System.Net;
 
 namespace Incubator.SocketClient
 {
-    interface IAddMoney
-    {
-        long AddMoney(long a, long b);
-    }
-
-    class Proxy : IDataContract
-    {
-        ulong _serviceHash;
-        RpcClient2 _client;
-
-        public Proxy(IPEndPoint endPoint)
-        {
-            _client = new RpcClient2(typeof(IDataContract), endPoint);
-            _serviceHash = CalculateHash(typeof(IDataContract).FullName);
-        }
-
-        public long AddMoney(long a, long b)
-        {
-            var ret = _client.InvokeMethod(_serviceHash, 1, new object[] { a, b });
-
-            return (long)ret[0];
-        }
-
-        private ulong CalculateHash(string str)
-        {
-            var hashedValue = 3074457345618258791ul;
-            for (var i = 0; i < str.Length; i++)
-            {
-                hashedValue += str[i];
-                hashedValue *= 3074457345618258799ul;
-            }
-            return hashedValue;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -58,7 +22,7 @@ namespace Incubator.SocketClient
             //Console.WriteLine(proxy.AddMoney(1, 2));
             //Console.WriteLine(s);
 
-            var p = new Proxy(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000));
+            var p = new DataContractProxy(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000));
             var s = p.AddMoney(1, 2);
             Console.WriteLine(s);
 
